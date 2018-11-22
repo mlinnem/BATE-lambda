ae3840a8940882490092fb054eadba099dfa9995
mlinnem <micah.linnemeier@gmail.com>
2018-11-22 15:20:02 -0600

    Doing createBallots properly now. Need to do corresponding work in submitBallot

--- lambda/createBallots/index.js
+++ lambda/createBallots/index.js
@@ -1,11 +1,15 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 
-var ddb = new AWS.DynamoDB({apiVersion: '2018-10-01'});
+//var ddb = new AWS.DynamoDB({apiVersion: '2018-10-01'});
+
+var io = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event) => {
     var authKey = event["queryStringParameters"]['authkey'];
 
+    console.log("authKey");
+    console.log(authKey);
     createBallots(authKey);
 }
 
@@ -14,26 +18,28 @@ exports.handler = (event) => {
 function createBallots(authKey) {
     return getAnimalsAndSession(authKey)
     .then(generateNewBallotsAndWriteToSession)
-    .then(returnBallots);
+    .then(returnBallots)
+    .catch(logError);
 };
 
 function getAnimalsAndSession(authkey) {
-    return Promise.all(getAnimals(), getSession(authkey));
+    return Promise.all([getAnimals(), getSession(authkey)]);
 }
 
 
 function generateNewBallotsAndWriteToSession(animals_and_session) {
-      var animals = animals_and_session[0]
+      console.log("generateNewBallotsAndWriteToSession");
+      console.log(animals_and_session);
+      var animals = animals_and_session[0];
       var session = animals_and_session[1];
-
-      var newBallots = [];
 
       var animalCount = getAnimalCount(animals);
 
       var newBallotsNeeded = calculateBallotsToProvide(session);
 
       var newBallots = generateNewBallots(newBallotsNeeded, animalCount);
-      writeNewBallotsToSession(newBallots, session);
+      writeNewBallotsToSession(newBallots, session)
+      .catch(logError);
 
       return newBallots;
 }
@@ -56,14 +62,17 @@ function returnBallots(ballots) {
 function writeNewBallotsToSession(newBallots, session) {
   //TODO: This can maybe occur asynchronously with respect to just returning the ballots to the user.
   //BUT they have to be written before the user tries to submit the ballot, otherwise backend will think it's bogus.
-    var updatedSession =  Object.assign({}, session, {PendingBallots" : session.PendingBallots.concat(newBallots)});
+    ///var updatedSession =  Object.assign({}, session, {"PendingBallots" : session.PendingBallots.concat(newBallots)});
+    console.log("TOP writeNEwBallotsToSession");
+    session.Item.PendingBallots = session.Item.PendingBallots.concat(newBallots);
+     console.log("UNDER writeNEwBallotsToSession");
 
     var put_params = {
-      Item: updatedSession,
+      Item: session.Item,
       TableName: 'AuthKey_To_Ballots'
     };
 
-    var request = ddb.putItem(put_params);
+    var request = io.put(put_params);
     var promise = request.promise();
     return promise;
 }
@@ -71,31 +80,33 @@ function writeNewBallotsToSession(newBallots, session) {
 function getAnimals() {
   var get_params = {
   Key: {
-   "ID": {
-     S: "0",
-    },
+   "ID": "0",
   },
   TableName: "AllAnimals"
  };
 
- var request = ddb.getItem(get_params);
+ console.log("Get Animals params");
+ console.log(get_params);
+ var request = io.get(get_params);
  var promise = request.promise();
- return promise.then(unmarshall);
+ return promise;
 }
 
 function getSession(authkey) {
+    console.log("Authkey in session");
+    console.log(authkey);
     var get_params = {
   Key: {
-   "ID": {
-     S: authkey,
-    },
+   "AuthKey": authkey,
   },
   TableName: "AuthKey_To_Ballots"
  };
 
- var request = ddb.getItem(get_params);
+ console.log("Get Session params");
+ console.log(get_params);
+ var request = io.get(get_params);
  var promise = request.promise();
- return promise.then(unmarshall);
+ return promise;
 }
 
 //--Utility functions--
@@ -104,6 +115,8 @@ function generateNewBallots(newBallotsNeeded, animalCount) {
         for (var i=0; i < newBallotsNeeded; i++) {
             var animal_1_num = 0;
             var animal_2_num = 0;
+
+                        var newBallots = [];
             //TODO: This will become a problem if the valid ID list is ever sparse (i.e. we remove an ID).
             animal_1_num = getRandomInt(0, animalCount - 1);
             if (animal_1_num > 0) {
@@ -117,8 +130,7 @@ function generateNewBallots(newBallotsNeeded, animalCount) {
                 animal_1_num = animal_2_num;
                 animal_2_num = temp;
               }
-            }
-            var ballot = [animal_1_num, animal_2_num];
+               var ballot = [animal_1_num, animal_2_num];
             newBallots.push(ballot);
         }
         return newBallots;
@@ -129,19 +141,27 @@ function getRandomInt(min, max) {
 }
 
 function calculateBallotsToProvide(session) {
-  return 15 - session.PendingBallots.length;
+  var num = 15 - session.Item.PendingBallots.length
+  console.log("CURRENTLY WAITING ON " + session.Item.PendingBallots.length + " BALLOTS");
+  console.log("GENERATING AND SENDING " + num + " NEW ONES");
+  return num;
 }
 
 function getAnimalCount(animals) {
-  var animalMap = animals.Item.Animals.M;
+  console.log("Animals in animal count");
+  console.log(animals);
+  var animalMap = animals.Item.Animals;
   var count = Object.keys(animalMap).length;
   return count;
 }
 
-function marshall(object) {
-  return AWS.DynamoDB.Converter.marshall(object);
+
+function logError(error) {
+    console.error(error);
 }
 
-function unmarshall(object) {
-  return AWS.DynamoDB.Converter.unmarshall(object);
+function printOutput(object) {
+    console.log("print Output: " + typeof object);
+    console.log(object);
+    return object;
 }
