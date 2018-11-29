05c58b4b2ad20f96c2220f72cdbcd781c3a24ae9
mlinnem <micah.linnemeier@gmail.com>
2018-11-28 20:00:36 -0600

    More work on IP Data

--- lambda/submitBallot/index.js
+++ lambda/submitBallot/index.js
@@ -2,17 +2,23 @@ var AWS = require('aws-sdk');
 // Set the region
 AWS.config.update({region: 'us-east-1'});
 
+const SHADOW_BANNED = "SHADOW_BANNED";
+const SHADOWBAN_SUBMISSION_THRESHOLD = 800
+const ACCEPTABLE_ABANDON_RATE = .8;
+const ACCEPTABLE_CONTRARIAN_RATE = .8;
+const WATCH_LISTED = "WATCH_LISTED";
+const BALLOT_ABANDONER = "BALLOT_ABANDONER";
+const CONTRARIAN = "CONTRARIAN";
 
 // Create an SQS service object
+
 var sqs = new AWS.SQS();
-var ddb = new AWS.DynamoDB({apiVersion: '2018-10-01'});
-var io = new AWS.DynamoDB.DocumentClient();
+var io = new AWS.DynamoDB.DocumentClient({apiVersion: '2018-10-01'}));
 
 exports.handler = (event, context, callback) => {
   console.log("event:");
   console.log(event);
   var data = event.body;
-  console.log(data);
   var parsedData = JSON.parse(data);
 
   var winnerID = parseInt(parsedData.WinnerID);
@@ -28,55 +34,65 @@ exports.handler = (event, context, callback) => {
     "ID": ballotID
   };
 
-  var ballot_and_authKey = [ballot, authKey];
+  var context = {
+    "ballot" : ballot,
+    "authKey" : authKey,
+    "ipAddress" : ipAddress,
+  }
 
-
-  return silentlyFailIfOnShadowbanList(ipAddress)
-  .then(submitBallot(ballot_and_authKey, callback))
-  .then(doIPBehaviorBookkeeping(ipAddress))
+  return getIPData(context)
+  .then(silentlyFailIfOnShadowbanList)
+  .then(deletePendingBallotOrFail)
+  .then(wrapEverythingUp)
   .catch(handleError);
 }
 
-function silentlyFailIfOnShadowbanList(ipAddress) {
-    var status = await backend_getIPAddressStatus(ipAddress);
-    if (status == "SHADOWBANNED") {
-      throw "Screw you buddy";
-    } else {
-      return null;
-    }
+function getIPData(context) {
+  return backend_getIPData(context.ipAddress)
+  .then((result) => {
+    context.ipData = result;
+    return context;
+  });
 }
 
-function submitBallot(ballot_and_authKey, callback) {
-    function () => {
-      recordBallotIfValid(ballot_and_authKey)
-      .then(prepareResponse(callback))
-      .catch(handleError); //TODO: Better error message when ballot is bogus?
-    }
-}
-
-function recordBallotIfValid(ballot_and_authKey_and_IP) {
-  //TODO: Produce an error if deleting something that doesn't exist using conditional dynamodb crap.
-  var ballot = ballot_and_authKey[0];
-  return backend_deletePendingBallot(ballot_and_authKey_IP)
-  .then(doIPBehaviorBookkeeping)
-  .then(backend_addBallotToQueueForProcessing(ballot)); //TODO: These can be parallelized later if needed.
-}
-
-function doIPBehaviorBookkeeping(ipAddress) {
-  return (ballot_and_authKey) => {
-      var ballot = ballot_and_authKey[0];
-      return backend_updateWatchlistBookkeeping(ipAddress, isContrarianBallot(ballot))
-      .then(addToShadowbanListIfWarranted);
+function silentlyFailIfOnShadowbanList(context) {
+  var status = context.ipData.status;
+  if (status == SHADOW_BANNED) {
+    throw "Not today buddy";
+  } else {
+  return context;
   }
 }
 
-const SHADOWBAN_SUBMISSION_THRESHOLD = 1600
-const ACCEPTABLE_ABANDON_RATE = .8;
-const ACCEPTABLE_CONTRARIAN_RATE = .8;
+function deletePendingBallotOrFail(context) {
+  return backend_deletePendingBallots(context.authKey, context.ballot)
+  .then((result) => {
+    return context;
+  });
+}
 
-function addToShadowbanListIfWarranted(updateResult) {
+function wrapEverythingUp(context) {
+  return Promise.all([backend_recordBallot(context.ballot),
+    updateIPData(context.ipAddress, context.ballot),
+    generateStandardSuccessResponse])
+  .then((results) => {
+    standardSuccessResponse = results[2];
+    return standardSuccessResponse;
+  });
+}
+
+function updateIPData(ipAddress, ballot) {
+  return backend_incrementAndGetIPData(context.ipAddress, context.ballot)
+  .then((ipData) => {
+    if warrantsShadowban(ipData) {
+      backend_setIPToShadowban(ipAddress);
+    }
+  });
+}
+
+function addToShadowbanListIfWarranted(ipData) {
   console.log("updateToIPAddressStuff:");
-  console.log(updateResult);
+  console.log(ipData);
 
   var submissionCount = 0;
   var contrarianSubmissions = 0;
@@ -88,17 +104,25 @@ function addToShadowbanListIfWarranted(updateResult) {
     var abandonRate = ballotsAbandoned / (ballotsAbandoned + ballotsNotAbandoned);
     var contrarianRate = contrarianSubmissions / (contrarianSubmissions + nonContrarianSubmissions);
 
+    var newStatus = WATCH_LISTED;
+    var reason = null;
+    if (abandonRate >  ACCEPTABLE_ABANDON_RATE) {
+      newStatus = SHADOW_BANNED;
+      reason = BALLOT_ABANDONER;
+    } else if (contrarianRate > ACCEPTABLE_CONTRARIAN_RATE) {
+      newStatus = SHADOW_BANNED;
+      reason = CONTRARIAN;
+    }
 
+    if (newStatus == SHADOW_BANNED) {
+      return backend_setIPTOShadowban(ipAddress, reason);
+    }
   }
-
-  //   If submissionCount > 1600 && (isContrarian || ballotAbandoner): (IP address -> voteCount, IP address to contrary/not count, IP address -> ballots abandoned vs not)
-  //     Add to ban list. (Write to status)
 }
 
-function prepareResponse(callback) {
+function generateStandardSuccessResponse(callback) {
     return () => {
       var responseBody = {
-        "foo": "bar", //TODO: No
       };
 
       var response = {
@@ -109,7 +133,7 @@ function prepareResponse(callback) {
           "Access-Control-Allow-Methods": '*'
         },
         "body": JSON.stringify(responseBody),
-        "isBase64Encoded": false
+        "isBase64Encoded": false;
       };
       callback(null, response);
     };
@@ -117,7 +141,8 @@ function prepareResponse(callback) {
 
   //--Backend functions--
 
-  function backend_addBallotToQueueForProcessing(ballot) {
+  function backend_recordBallot(ballot) {
+    return () => {
     console.log("ADDING BALLOT TO QUEUE FOR PROCESSING");
     console.log("params:");
     var params = {
@@ -145,9 +170,10 @@ function prepareResponse(callback) {
           console.log(data);
         }
       });
-  }
+  };
+}
 
-  function backend_deletePendingBallot(ballot_and_authKey) {
+  function backend_deletePendingBallotOrFail(ballot_and_authKey) {
     console.log("DELETING SUBMITTED BALLOT FROM PENDING BALLOTS");
     var submittedBallot = ballot_and_authKey[0];
     console.log("submittedBallot:");
@@ -165,7 +191,7 @@ function prepareResponse(callback) {
     return io.delete(delete_params).promise();
   }
 
-  function backend_updateWatchlistBookkeeping(ipAddress, isContrarian) {
+  function backend_incrementIPData(ipAddress, isContrarian) {
     var contrarianIncrement = 0;
     var nonContrarianIncrement = 0;
     if (isContrarian) {
@@ -183,15 +209,21 @@ function prepareResponse(callback) {
         "IPAddress" : ipAddress
       },
       "UpdateExpression" : 'SET Submissions = Submissions + :s_inc, ContrarianSubmissions = ContrarianSubmissions + :c_inc, NonContrarianSubmissions = NonContrarianSubmissions + :n_inc, BallotsNotAbandoned = BallotsNotAbandoned + :b_inc',
-      "ExpressionAttributeValues" : {"s_inc" : 1, "c_inc" : contrarianIncrement, "n_inc" : nonContrarianIncrement, "b_inc" : 1}
+      "ExpressionAttributeValues" : {"s_inc" : 1, "c_inc" : contrarianIncrement, "n_inc" : nonContrarianIncrement, "b_inc" : 1},
       "ReturnValues" : "ALL_NEW"
       }
 
-    return io.update(updateParams).promise();
+    return io.update(updateParams).promise()
+    .then((result) {
+      console.log("result of updated IP data");
+      console.log(result);
+      if (isShadowBanWorthy(result)) {
+        backend_setIPToShadowban(ipAddress);
+      }
+    }
   }
 
   //--Utility functions--
-
 
   function handleError(error) {
     console.error(error);
